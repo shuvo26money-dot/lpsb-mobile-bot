94de05c228e7e454dd56fd66d4e059d572f3afdb
shuvo26money-dot <shuvoahmed16121@gmail.com>
2026-07-24 07:11:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 import time
-
 from signal import generate_signal
-from telegram_bot import send_message
-
+from telegram import send_message
 
 print("🚀 LPSB PRO BOT STARTED")
 
 while True:
-
     try:
-
         message = generate_signal()
 
         if message:
-
             print(message)
-
             send_message(message)
-
         else:
-
-            print("⏳ No strong setup...")
+            print("⏳ No strong setup")
 
     except Exception as e:
+        print(f"❌ Error: {e}")
 
-        print(f"❌ Bot Error: {e}")
-
-    # প্রতি 120 সেকেন্ডে একবার চেক করবে
     time.sleep(120)
